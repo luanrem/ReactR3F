22addfa912e7a0e1f31a98f41316c60fcc581d73
luanrem <luanrem@gmail.com>
2023-03-15 20:49:16 -0300

    feat: initial animation

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,16 +1,27 @@
-import React from "react";
+import React, { useRef } from "react";
+import { useFrame } from '@react-three/fiber'
 
 export default function Experience() {
+  const cubeRef = useRef()
+  const groupRef = useRef()
+
+  useFrame((state, delta) => {
+    cubeRef.current.rotation.y += delta
+    groupRef.current.rotation.y += delta
+  })
+
   return (
     <>
-      <mesh position-x={-2}>
-        <sphereGeometry />
-        <meshBasicMaterial color='orange' />
-      </mesh>
-      <mesh rotation-y={Math.PI * 0.25} position-x={2} scale={1.5}>
-        <boxGeometry />
-        <meshBasicMaterial color="mediumpurple" />
-      </mesh>
+      <group ref={groupRef}>
+        <mesh position-x={-2}>
+          <sphereGeometry />
+          <meshBasicMaterial color='orange' />
+        </mesh>
+        <mesh ref={cubeRef} rotation-y={Math.PI * 0.25} position-x={2} scale={1.5}>
+          <boxGeometry />
+          <meshBasicMaterial color="mediumpurple" />
+        </mesh>
+      </group>
       <mesh position-y={- 1} rotation-x={-Math.PI * 0.5} scale={10} >
         <planeGeometry />
         <meshBasicMaterial color="greenyellow" />
