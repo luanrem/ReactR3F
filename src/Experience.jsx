2d7a71f86f6c59a3d6f1608d02ae5ebb8ec01157
luanrem <luanrem@gmail.com>
2023-03-19 15:16:43 -0300

    refactor: Lesson43 starter pack

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,59 +1,32 @@
-import React, { useRef } from "react";
-import { useFrame, extend, useThree } from '@react-three/fiber'
+import { useThree, extend } from '@react-three/fiber'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-import CustomObject from "./CustomObject";
 
 extend({ OrbitControls })
 
 export default function Experience() {
-  const cubeRef = useRef()
-  const groupRef = useRef()
-
   const { camera, gl } = useThree()
 
-  useFrame((state, delta) => {
+  return <>
 
-    // console.log(state.camera.position)
-    // console.log(state.clock.elapsedTime)
+    <orbitControls args={[camera, gl.domElement]} />
 
-    // const angle = state.clock.elapsedTime
-    // state.camera.position.x = Math.sin(angle) * 8
-    // state.camera.position.z = Math.cos(angle) * 8
-    // state.camera.lookAt(0, 0, 0)
+    <directionalLight position={[1, 2, 3]} intensity={1.5} />
+    <ambientLight intensity={0.5} />
 
-    cubeRef.current.rotation.y += delta
-    // groupRef.current.rotation.y += delta
-  })
+    <mesh position-x={- 2}>
+      <sphereGeometry />
+      <meshStandardMaterial color="orange" />
+    </mesh>
 
-  return (
-    <>
-      <orbitControls args={[camera, gl.domElement]} />
+    <mesh position-x={2} scale={1.5}>
+      <boxGeometry />
+      <meshStandardMaterial color="mediumpurple" />
+    </mesh>
 
-      <directionalLight position={[1, 2, 3]} intensity={1.5} />
-      <ambientLight intensity={0.5} />
+    <mesh position-y={- 1} rotation-x={- Math.PI * 0.5} scale={10}>
+      <planeGeometry />
+      <meshStandardMaterial color="greenyellow" />
+    </mesh>
 
-      <group ref={groupRef}>
-        <mesh position-x={-2}>
-          <sphereGeometry />
-          <meshStandardMaterial color='orange' />
-        </mesh>
-        <mesh ref={cubeRef} rotation-y={Math.PI * 0.25} position-x={2} scale={1.5}>
-          <boxGeometry />
-          <meshStandardMaterial color="mediumpurple" />
-        </mesh>
-      </group>
-      <mesh position-y={- 1} rotation-x={-Math.PI * 0.5} scale={10} >
-        <planeGeometry />
-        <meshStandardMaterial color="greenyellow" />
-      </mesh>
-      <group position={[0, -1, 0]}>
-        <mesh position-y={2}>
-          <boxGeometry />
-          <meshBasicMaterial color={'#00ffff'} />
-        </mesh>
-      </group>
-
-      <CustomObject />
-    </>
-  );
+  </>
 }
--- src/index.jsx
+++ src/index.jsx
@@ -1,27 +1,18 @@
 import './style.css'
 import ReactDOM from 'react-dom/client'
 import { Canvas } from '@react-three/fiber'
-import Experience from './Experience'
-import { ACESFilmicToneMapping, LinearEncoding } from 'three'
+import Experience from './Experience.jsx'
 
 const root = ReactDOM.createRoot(document.querySelector('#root'))
 
-const cameraSettings = {
-    fov: 45,
-    near: 0.1,
-    far: 200,
-    position: [3, 2, 6]
-}
-
 root.render(
     <Canvas
-        dpr={[1, 2]}
-        gl={{
-            antialias: true,
-            toneMapping: ACESFilmicToneMapping,
-            // outputEncoding: LinearEncoding
+        camera={{
+            fov: 45,
+            near: 0.1,
+            far: 200,
+            position: [- 4, 3, 6]
         }}
-        camera={cameraSettings}
     >
         <Experience />
     </Canvas>
