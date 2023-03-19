36712c7ebe6f37dd7bb2a9fa58b6d3e01205ef17
luanrem <luanrem@gmail.com>
2023-03-19 15:37:58 -0300

    feat: transformControls

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,10 +1,12 @@
-import { OrbitControls } from '@react-three/drei'
+import { OrbitControls, TransformControls } from '@react-three/drei'
+import { useRef } from 'react'
 
 export default function Experience() {
+  const cube = useRef()
 
   return <>
 
-    <OrbitControls enableDamping={true} />
+    <OrbitControls makeDefault enableDamping={true} />
 
     <directionalLight position={[1, 2, 3]} intensity={1.5} />
     <ambientLight intensity={0.5} />
@@ -14,10 +16,11 @@ export default function Experience() {
       <meshStandardMaterial color="orange" />
     </mesh>
 
-    <mesh position-x={2} scale={1.5}>
+    <mesh ref={cube} position-x={2} scale={1.5}>
       <boxGeometry />
       <meshStandardMaterial color="mediumpurple" />
     </mesh>
+    <TransformControls object={cube} mode="translate" />
 
     <mesh position-y={- 1} rotation-x={- Math.PI * 0.5} scale={10}>
       <planeGeometry />
