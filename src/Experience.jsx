a2265a75cc1a66e66abd703636792e5259006560
luanrem <luanrem@gmail.com>
2023-03-27 21:13:24 -0300

    debug: debug ui with Leva

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,11 +1,36 @@
 import { OrbitControls } from '@react-three/drei'
-import { useControls } from 'leva'
+import { useControls, button } from 'leva'
 
 export default function Experience() {
-  const controls = useControls({
-    position: - 2
+  const { position, color, visible } = useControls('sphere', {
+    position: {
+      value: { x: -2, y: 0 },
+      step: 0.01,
+      joystick: 'invertY'
+    },
+    color: '#ff0000',
+    visible: true,
+    myInterval: {
+      min: 0,
+      max: 10,
+      value: [4, 5]
+    },
+    clickMe: button(() => {
+      console.log('ok')
+    }),
+    choice: {
+      options: ['a', 'b', 'c']
+    }
   })
-  console.log(controls)
+
+  const { cubeScale } = useControls('cube', {
+    cubeScale: {
+      value: 1.5,
+      min: 0.1,
+      max: 3,
+      step: 0.1
+    }
+  })
 
   return <>
 
@@ -14,12 +39,12 @@ export default function Experience() {
     <directionalLight position={[1, 2, 3]} intensity={1.5} />
     <ambientLight intensity={0.5} />
 
-    <mesh position-x={- 2}>
+    <mesh position={[position.x, position.y, 0]} visible={visible}>
       <sphereGeometry />
-      <meshStandardMaterial color="orange" />
+      <meshStandardMaterial color={color} />
     </mesh>
 
-    <mesh position-x={2} scale={1.5}>
+    <mesh position-x={2} scale={cubeScale}>
       <boxGeometry />
       <meshStandardMaterial color="mediumpurple" />
     </mesh>
